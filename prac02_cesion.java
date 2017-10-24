9018877fe6288610a4fc2d6e91f27845f77bbb90
alinex410 <32894441+alinex410@users.noreply.github.com>
2017-10-24 22:05:39 +0200

    Prac02 cesion.cpp modificación 1

--- prac02_cesion.java
+++ prac02_cesion.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lab1;
+package prac02;
 
 import java.util.ArrayList;
 import java.util.Date;
